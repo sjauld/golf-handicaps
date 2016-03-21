274d21894926b14a04de3d0ec9add3fa363d3d29
Stuart Auld <sauld@cozero.com.au>
2016-03-21 21:15:45 +1100

    round update methods

--- models/round.rb
+++ models/round.rb
@@ -7,7 +7,8 @@ class Round < ActiveRecord::Base
     # stand alone round?
     if self.game.nil?
       update_attribute(:daily_scratch_rating,self.tee.acr)
-      update_attribute(:adjusted_gross_score,self.tee.par + self.playing_handicap + 36 + self.gross_stableford_score)
+      update_attribute(:adjusted_gross_score,self.tee.par + self.playing_handicap + 36 - self.score)
+      update_attribute(:differential,(self.adjusted_gross_score - self.daily_scratch_rating) * 113 / self.tee.slope)
     # otherwise, apply formula and update all rounds
     else
       all_rounds = Game.find(self.game_id)
