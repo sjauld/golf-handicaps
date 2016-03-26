69b060a8c39f2785dca7aee68ba9a8b77e3cb5cb
Stuart Auld <sauld@cozero.com.au>
2016-03-26 11:43:52 +1100

    bulk imports and round delete functionality

--- routes/round.rb
+++ routes/round.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class App < Sinatra::Base
 
   before '/round/*' do
@@ -25,4 +27,46 @@ class App < Sinatra::Base
     flash[:notice] = 'Round added'
     redirect "/round/add?tee=#{params['tee']}"
   end
+
+  # bulk imports
+  get '/round/import' do
+    haml :'/round/import'
+  end
+
+  post '/round/import' do
+    data = params["file"][:tempfile].read
+    count = 0
+    errors = 0
+    CSV.parse(data, headers: true).each do |row|
+      begin
+        round = User.find(row['User ID']).rounds.create(
+          played_date: Date.parse(row['Date']),
+          playing_handicap: row['Played off'].to_i,
+          format: row['Comp'],
+          score: row['Score'],
+          tee_id: row['Tee ID']
+        )
+        round.update_round_statistics
+        count += 1
+      rescue => e
+        puts "#{e.class}: #{e.message}"
+        errors += 1
+      end
+    end
+    flash[:notice] = "Added #{count} rounds!" unless count == 0
+    flash[:error] = "#{errors} errors occurred!" unless errors == 0
+    redirect '/'
+  end
+
+  get '/round/:id/delete' do
+    round = Round.find(params[:id])
+    if round.user.id == @user.id || @user.id == 1
+      round.delete
+      flash[:notice] = 'Success'
+    else
+      flash[:notice] = 'You are not authorised to delete that round'
+    end
+    redirect '/'
+  end
+
 end
