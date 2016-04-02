1896e2178c540b19ae4a44ccb52a3a84f1186198
Stuart Auld <sauld@cozero.com.au>
2016-04-02 13:05:21 +1100

    user edit functionality

--- routes/user.rb
+++ routes/user.rb
@@ -15,17 +15,36 @@ class App < Sinatra::Base
   post '/users/add' do
     authorize
     nice_params = escape_html_for_set(params)
-    user = User.create({name: nice_params['name'], first_name: nice_params['first_name'], email: nice_params['email'], image: nice_params['image']})
+    user = User.create({name: nice_params['name'], first_name: nice_params['first_name'], email: nice_params['email'], sex: nice_params['sex']})
     flash[:notice] = 'User created successfully'
     redirect "/users/#{user.id}"
   end
 
   # refresh handicap
   get '/users/:id/refresh' do
-    authorize
     @this_user = User.find(params[:id])
+    authorize_can_edit_user(@this_user)
     @this_user.update_handicap
     flash[:notice] = 'Handicap refreshed'
+    redirect "/users/#{params[:id]}"
+  end
+
+  # profile edit
+  get '/users/:id/edit' do
+    @this_user = User.find(params[:id])
+    authorize_can_edit_user(@this_user)
+    haml :'user/edit_profile'
+  end
+
+  post '/users/:id/edit' do
+    @this_user = User.find(params[:id])
+    authorize_can_edit_user(@this_user)
+    nice_params = escape_html_for_set(params)
+    ['name','first_name','sex'].each do |x|
+      @this_user.send("#{x}=", nice_params[x]) unless nice_params[x].nil?
+    end
+    @this_user.save
+    flash[:notice] = 'User updated'
     redirect "/users/#{params[:id]}"
   end
 
